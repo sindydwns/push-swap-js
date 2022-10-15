e591bb0fbff76abc3df9655bd4e91731853ff191
sindydwns <sindydwns@naver.com>
2022-10-15 23:53:46 +0900

    update data.mjs

--- data.mjs
+++ data.mjs
@@ -1,27 +1,40 @@
 const data = {};
 data.swap = (container) => {
-
+	if (container.length < 2)
+		return;
+	const temp = container[container.length - 2];
+	container[container.length - 2] = container[container.length - 1];
+	container[container.length - 1] = temp;
 }
 
 data.push = (from, to) => {
-
+	if (from.length < 1)
+		return;
+	const temp = from.pop();
+	to.push(temp);
 }
 
 data.rotate = (container, isReverse) => {
-
+	if (container.length < 2)
+		return;
+	const temp = isReverse ? container.shift() : container.unshift();
+	if (isReverse)
+		container.unshift(temp);
+	else
+		container.push(temp);
 }
 
 data.a = [];
 data.b = [];
-data.sa = () => swap(a);
-data.sb = () => swap(b);
-data.ss = () => swap(a) & swap(b);
-data.pa = () => push(b, a);
-data.pb = () => push(a, b);
-data.ra = () => rotate(a, false);
-data.rb = () => rotate(b, false);
-data.rr = () => rotate(a, false) & rotate(b, false);
-data.rra = () => rotate(a, true);
-data.rrb = () => rotate(b, true);
-data.rrr = () => rotate(a, true) & rotate(b, true);
+data.sa = () => data.swap(data.a);
+data.sb = () => data.swap(data.b);
+data.ss = () => data.swap(data.a) & data.swap(b);
+data.pa = () => data.push(data.b, data.a);
+data.pb = () => data.push(data.a, data.b);
+data.ra = () => data.rotate(data.a, false);
+data.rb = () => data.rotate(data.b, false);
+data.rr = () => data.rotate(data.a, false) & data.rotate(data.b, false);
+data.rra = () => data.rotate(data.a, true);
+data.rrb = () => data.rotate(data.b, true);
+data.rrr = () => data.rotate(data.a, true) & data.rotate(data.b, true);
 export default data;
