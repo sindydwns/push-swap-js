4979148effc64e9afbbb0a384532148290b8b2e7
sindydwns <sindydwns@naver.com>
2022-10-18 13:35:58 +0900

    class data

--- data.mjs
+++ data.mjs
@@ -1,40 +1,41 @@
-const data = {};
-data.swap = (container) => {
-	if (container.length < 2)
-		return;
-	const temp = container[container.length - 2];
-	container[container.length - 2] = container[container.length - 1];
-	container[container.length - 1] = temp;
-}
+class Data {
+	constructor(a, b) {
+		this.a = a;
+		this.b = b;
+	}
+	swap(container) {
+		if (container.length < 2)
+			return;
+		const temp = container[container.length - 2];
+		container[container.length - 2] = container[container.length - 1];
+		container[container.length - 1] = temp;
+	}
+	push(from, to) {
+		if (from.length < 1)
+			return;
+		const temp = from.pop();
+		to.push(temp);
+	}
+	rotate(container, isReverse) {
+		if (container.length < 2)
+			return;
+		const temp = isReverse ? container.shift() : container.pop();
+		if (isReverse)
+			container.push(temp);
+		else
+			container.unshift(temp);
+	}
 
-data.push = (from, to) => {
-	if (from.length < 1)
-		return;
-	const temp = from.pop();
-	to.push(temp);
+	sa() { this.swap(this.a); }
+	sb() { this.swap(this.b); }
+	ss() { this.swap(this.a) & this.swap(this.b); }
+	pa() { this.push(this.b, this.a); }
+	pb() { this.push(this.a, this.b); }
+	ra() { this.rotate(this.a, false); }
+	rb() { this.rotate(this.b, false); }
+	rr() { this.rotate(this.a, false) & this.rotate(this.b, false); }
+	rra() { this.rotate(this.a, true); }
+	rrb() { this.rotate(this.b, true); }
+	rrr() { this.rotate(this.a, true) & this.rotate(this.b, true); }
 }
-
-data.rotate = (container, isReverse) => {
-	if (container.length < 2)
-		return;
-	const temp = isReverse ? container.shift() : container.pop();
-	if (isReverse)
-		container.push(temp);
-	else
-		container.unshift(temp);
-}
-
-data.a = [];
-data.b = [];
-data.sa = () => data.swap(data.a);
-data.sb = () => data.swap(data.b);
-data.ss = () => data.swap(data.a) & data.swap(data.b);
-data.pa = () => data.push(data.b, data.a);
-data.pb = () => data.push(data.a, data.b);
-data.ra = () => data.rotate(data.a, false);
-data.rb = () => data.rotate(data.b, false);
-data.rr = () => data.rotate(data.a, false) & data.rotate(data.b, false);
-data.rra = () => data.rotate(data.a, true);
-data.rrb = () => data.rotate(data.b, true);
-data.rrr = () => data.rotate(data.a, true) & data.rotate(data.b, true);
-export default data;
+export default Data;
